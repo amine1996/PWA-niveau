0d4061c7e4c689b34d32c470dc3b39ee52f1edf4
amine1996 <amine_ismail@hotmail.fr>
2018-04-05 15:13:50 +0200

    Suicide mental

--- src/js/sketch.js
+++ src/js/sketch.js
@@ -1,11 +1,8 @@
-//Last orientation (PORTRAIT or LANDSCAPE)
-var lastOrientation;
-
 //Last Y rotation
-var lastRotationY;
+var horizontalBubbleAngle;
 
 //Last X rotation
-var lastRotationX;
+var verticalBubbleAngle;
 
 const DEBUG = true;
 
@@ -13,15 +10,11 @@ function setup()
 {
   createCanvas(screen.width, screen.height);
 
-  if(deviceOrientation == LANDSCAPE)
-    resizeCanvas(screen.height, screen.width);
-
   background(255,255,255);   
   angleMode(DEGREES);
 
-  lastOrientation = deviceOrientation;
-  lastRotationY = 0; 
-  lastRotationX = 0;
+  horizontalBubbleAngle = 0; 
+  verticalBubbleAngle = 0;
 
   textSize(20);
 }
@@ -35,22 +28,19 @@ function draw()
   {
     //Horizontal bubble
     //Not working properly
-    let deltaRotationY = cos(rotationY) - cos(lastRotationY);
-    if(rotationY < 0)
-      lastRotationY += map(deltaRotationY,-2,2,-5,5)
-    else
-      lastRotationY -= map(deltaRotationY,-2,2,-5,5)
+    let deltaRotationY = sin(rotationY) - sin(horizontalBubbleAngle);
+    horizontalBubbleAngle += map(deltaRotationY,-2,2,-5,5)
 
     fill(0,0,255);
-    ellipse(screen.width*map(cos(lastRotationY+90),-1,1,0.05,0.95),screen.height/2, 80, 80);
+    ellipse(screen.width*map(cos(horizontalBubbleAngle+90),-1,1,0.05,0.95),screen.height/2, 80, 80);
 
     //Vertical bubble
     //OK
-    let deltaRotationX = sin(rotationX) - sin(lastRotationX);
-    lastRotationX += map(deltaRotationX,-2,2,-5,5)
+    let deltaRotationX = sin(rotationX) - sin(verticalBubbleAngle);
+    verticalBubbleAngle += map(deltaRotationX,-2,2,-5,5)
 
     fill(255,0,0);
-    ellipse(screen.width/2, screen.height*map(cos(lastRotationX+90),-1,1,0.05,0.95), 80, 80);
+    ellipse(screen.width/2, screen.height*map(cos(verticalBubbleAngle+90),-1,1,0.05,0.95), 80, 80);
 
         
     if(DEBUG)
@@ -60,25 +50,12 @@ function draw()
       text("RotationY : "+rotationY,50,70);
       text("RotationZ : "+rotationZ,50,100);
 
-      text("lastRotationX : "+lastRotationX,50,140);
-      text("lastRotationY : "+lastRotationY,50,170);
+      text("verticalBubbleAngle : "+verticalBubbleAngle,50,140);
+      text("horizontalBubbleAngle : "+horizontalBubbleAngle,50,170);
     }
   }
   else
   {
     text("No rotation found",10,30);
-  }
-}
-//Not used for the moment-
-function resizeOnOrientationChange()
-{
-  if(lastOrientation != deviceOrientation)
-  {
-    if(deviceOrientation == LANDSCAPE)
-      resizeCanvas(screen.height,screen.width);
-    else
-      resizeCanvas(screen.width,screen.height);
-
-    lastOrientation = deviceOrientation;
   }
 }
